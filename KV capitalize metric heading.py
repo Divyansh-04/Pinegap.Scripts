8cf404b8df4f55b599bcaa39df063b41a21c6354
Divyansh Goel <divyanshgoel2004@gmail.com>
2025-03-26 02:42:54 +0530

    'by' exception in titlecasing

--- KV capitalize metric heading.py
+++ KV capitalize metric heading.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
-sheet_path = r"C:\Users\Divya\Downloads\Europe - Germany - KeyValues.csv"
+sheet_path = r"C:\Users\Divya\Downloads\STox-600 - Key Values .csv"
+dest_path =r'.\data\updated stox KV.csv'
+
 df = pd.read_csv(sheet_path)
 
 print(df['Metric Heading'])
 
 for i in range(len(df)):
     if isinstance(df.at[i,'Metric Heading'], str):
-        df.at[i, 'Metric Heading'] = ' '.join(df.at[i, 'Metric Heading'].title().split())
+        df.at[i, 'Metric Heading'] = ' '.join(x if x.lower() != 'by' else x.lower() for x in df.at[i, 'Metric Heading'].title().split())
 
-df.to_csv(r'.\data\updated KV.csv', index=False)
+df.to_csv(dest_path, index=False)
