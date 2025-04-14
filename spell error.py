cb54bd04664a305492313852d6658067128a3298
Divyansh Goel <divyanshgoel2004@gmail.com>
2025-04-14 16:00:06 +0530

    yahoo scraper

--- spell error.py
+++ spell error.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 sheet_path = r"C:\Users\Divya\Downloads\China expansion - KeyValues(1-500).csv"
-dest_path =r'.\data\China 501-end titlecased.csv'
+dest_path =r'.\data\exp'
 
 df = pd.read_csv(sheet_path)
 
@@ -9,6 +9,6 @@ print(df['Metric Heading'].unique())
 
 for i in range(len(df)):
     if isinstance(df.at[i,'Metric Heading'], str):
-        df.at[i, 'Metric Heading'] = ' '.join(x if x.lower() != 'by' else x.lower() for x in df.at[i, 'Metric Heading'].title().split())
+        pass
 
 df.to_csv(dest_path, index=False)
